6be96ec52cb258c20847c2a878ebb1462fde6e0a
[inaju] <[mitchelinajuo@gmail.com]>
2023-07-13 18:42:42 +0100

    Added recent projects and worked on the blog component

--- components/Blog/Post.tsx
+++ components/Blog/Post.tsx
@@ -1,36 +1,42 @@
-import { PostType } from '@/data/content/blog';
-import Image from 'next/image';
-import { FC } from 'react';
+import { PostType } from "@/data/content/blog";
+import Image from "next/image";
+import { FC } from "react";
 
 interface Props {
-    post: PostType;
+  post: PostType;
 }
 
 export const Post: FC<Props> = ({ post }) => {
-    return (
-        <a
-            className="relative flex flex-col m-5 my-5 mt-5 rounded-lg md:mb-0 md:mx-5 w-80 md:w-96 dark:bg-darkerBlue"
-            href={`https://mitchel.hashnode.dev/${post.slug}`}
-            key={post._id}
-            rel="noreferrer"
-            target="_blank"
-        >
-            {post.coverImage && (
-                <Image
-                    alt={post.title}
-                    className="rounded-t-lg object-cover"
-                    height={786}
-                    src={post.coverImage}
-                    width={1500}
-                />
-            )}
+  return (
+    <a
+      className=" relative flex flex-col  sm:m-5 my-5 mt-5 sm:mt-10  rounded-lg md:mb-0 md:mx-5 w-80 md:w-[32rem] dark:bg-darkerBlue"
+      //   className="relative flex flex-col m-5 my-5 mt-5 rounded-lg md:mb-0 md:mx-5 w-80 md:w-96 dark:bg-darkerBlue"
+      href={`https://mitchel.hashnode.dev/${post.slug}`}
+      key={post._id}
+      rel="noreferrer"
+      target="_blank"
+    >
+      {post.coverImage && (
+        <div className=" mx-5 sm:mx-0 w-[18rem] sm:w-[20rem] md:w-[30rem]">
+          <img
+            alt={post.title}
+            className="rounded-t-lg object-cover  w-full h-full"
+            // height={786}
+            src={post.coverImage}
+            // width={1500}
+            //   layout="fill"
+          />
+        </div>
+      )}
 
-            <div className="flex flex-col p-4 pb-4 text-left">
-                <h2 className="text-lg font-medium text-white">{post.title}</h2>
-                <p className="mt-4 text-fun-gray text-base break-words text-text">
-                    {post.brief.substring(0, 100)}...
-                </p>
-            </div>
-        </a>
-    );
+      <div className=" mx-5 sm:mx-0 flex flex-col py-4 pb-4 text-left">
+        <h2 className="text-lg font-medium text-white hover:text-white/[0.5] ">
+          {post.title}
+        </h2>
+        <p className="mt-4 text-fun-gray text-base break-words text-text">
+          {post.brief.substring(0, 150)}...
+        </p>
+      </div>
+    </a>
+  );
 };
--- data/content/projects.ts
+++ data/content/projects.ts
@@ -4,6 +4,21 @@ import { Project } from "types";
 const projects: Project[] = [
   {
     id: 0,
+    title: "Zwilt Store",
+    desc: "Get access to Top Tech Talent worldwide",
+    img: "/static/projects/zwilt-store.png",
+    link: "https://staging.zwilt.com/",
+    tags: [
+      "NextJS",
+      "Typescript",
+      "NextJS",
+      "AWS",
+      "Material UI",
+      "Styled Components",
+    ],
+  },
+  {
+    id: 1,
     title: "Grazac Academy",
     desc: "Grazac Academy gives you the opportunity to learn in-demand skills to build your tech career.",
     img: "/static/projects/grazac_academy.png",
@@ -11,23 +26,23 @@ const projects: Project[] = [
     tags: ["React", "NextJS", "SASS", "HTML"],
   },
   {
-    id: 1,
+    id: 2,
     title: "Zwilt Application",
     desc: "Talent Acquisition Suite that makes it easy for talent teams to connect companies with top talent.",
     img: "/static/projects/zwilt_application.png",
     link: "https://application.zwilt.com/",
-    tags: ["Typescript","React", "NextJS", "MaterialUI", "CSS"],
+    tags: ["Typescript", "React", "NextJS", "MaterialUI", "CSS"],
   },
   {
-    id: 2,
+    id: 3,
     title: "Routelift",
     desc: "The Operating System for Last Mile Delivery Services.",
     img: "/static/projects/routelift.png",
     link: "https://Routelift.com/",
-    tags: ["Typescript","React", "NextJS", "HTML"],
+    tags: ["Typescript", "React", "NextJS", "HTML"],
   },
   {
-    id: 3,
+    id: 4,
     title: "Keepup",
     desc: "Cloud based Note taking application that enables professionals sync their notes across platforms, guaranteed to boot your productivity",
     img: "/static/projects/keepup-screen.png",
@@ -36,7 +51,7 @@ const projects: Project[] = [
     tags: ["Javascript", "Firebase", "NextJS", "HTML", "Tailwind"],
   },
   {
-    id: 4,
+    id: 5,
     title: "Vtuslot",
     desc: "Africa's first VTU trading platform, buy and sell on a simple and safe platform with trusted users.",
     img: "/static/projects/vtuslot.png",
@@ -44,17 +59,14 @@ const projects: Project[] = [
     github: "https://github.com/inaju/vtuslot",
     tags: ["React", "CSS", "Html"],
   },
-  
 ];
 
-export const allTags = []
+export const allTags = [];
 
 projects.forEach((project) => {
-  project.tags.forEach((tag) => !allTags.includes(tag) && allTags.push(tag))
+  project.tags.forEach((tag) => !allTags.includes(tag) && allTags.push(tag));
 });
 
-export const allKebabTags = allTags.map(tag => (
-  kebabCase(tag)
-))
+export const allKebabTags = allTags.map((tag) => kebabCase(tag));
 
-export default projects
+export default projects;
